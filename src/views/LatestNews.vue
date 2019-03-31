fc769fa2c0e95d7ea871a30fb0c622bb6422b98f
Taurin190 <taura.koichi@gmail.com>
2019-03-31 12:10:41 +0900

    call hacker news and show news list

--- src/views/LatestNews.vue
+++ src/views/LatestNews.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="latest_news">
-    <NewsList title="Latest Hacker News" />
+    <NewsList title="Latest Hacker News" :newsList="newsList" />
   </div>
 </template>
 <script>
 import NewsList from "@/components/NewsList.vue";
+import axios from 'axios';
 
 export default {
   name: "latest",
@@ -13,15 +14,44 @@ export default {
   },
   data() {
       return {
-          newsIds: [],
-          newsList: []
+          newsIds: []
+        //   newsList: []
+      }
+  },
+  mounted () {
+    axios
+      .get('https://hacker-news.firebaseio.com/v0/topstories.json')
+      .then(response => (this.newsIds = response));
+  },
+  computed: {
+      newsList() {
+          var list = [];
+          var count = 0;
+          if (this.newsIds.length == 0) return list;
+          for (var id of this.newsIds.data) {
+              console.log(id);
+              if (count++ > 9) return list;
+              var url = "https://hacker-news.firebaseio.com/v0/item/" + id + ".json";
+              axios
+              .get(url)
+              .then(response => (list.push(response)));
+          }
       }
   },
   methods: {
 
-  },
-  watch: {
-      
   }
+//   watch: {
+//       newsIds(idList, oldIdList) {
+//           for (var id of idList.data) {
+//               console.log(id);
+//               if (this.newsList.length > 10) return;
+//               var url = "https://hacker-news.firebaseio.com/v0/item/" + id + ".json";
+//               axios
+//               .get(url)
+//               .then(response => (this.newsList.push(response)));
+//           }
+//       }
+//   }
 };
 </script>
